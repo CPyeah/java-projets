becd71890dfdcf0cdc78e6a110a8717f5bab4bd8
chengpeng <chengpengyeah@126.com>
2022-05-22 17:11:08 +0800

    HyperLogLogTests

--- java-redis/src/test/java/org/cp/javaredis/HyperLogLogTests.java
+++ java-redis/src/test/java/org/cp/javaredis/HyperLogLogTests.java
@@ -42,4 +42,26 @@ public class HyperLogLogTests {
 		System.out.println("set count: " + set.size());
 	}
 
+	@Test
+	public void mergeTest() {
+		RHyperLogLog<String> today = redisson.getHyperLogLog("UV_" + LocalDate.now());
+		RHyperLogLog<String> nextDay = redisson.getHyperLogLog("UV_" + LocalDate.now().plusDays(1));
+		today.delete();
+		HashSet<String> set = new HashSet<>();
+		Random random = new Random();
+		for (int i = 0; i < 1000; i++) {
+			String userId = "id_" + random.nextInt(1000);
+			today.add(userId);
+			set.add(userId);
+		}
+		for (int i = 0; i < 1000; i++) {
+			String userId = "id_" + random.nextInt(2000);
+			nextDay.add(userId);
+			set.add(userId);
+		}
+		today.mergeWith(nextDay.getName());
+		System.out.println("RHyperLogLog: " + today.count());
+		System.out.println("set count: " + set.size());
+	}
+
 }
