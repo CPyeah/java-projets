b0b08e13df8bca6b9be6f38c29d322c40728d345
chengpeng <chengpengyeah@126.com>
2022-05-22 16:20:10 +0800

    redis queue

---
+++ java-redis/src/test/java/org/cp/javaredis/QueueTest.java
@@ -0,0 +1,75 @@
+package org.cp.javaredis;
+
+import java.util.concurrent.TimeUnit;
+import org.junit.jupiter.api.Test;
+import org.redisson.api.RBlockingQueue;
+import org.redisson.api.RDelayedQueue;
+import org.redisson.api.RQueue;
+import org.redisson.api.RedissonClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class QueueTest {
+
+	@Autowired
+	RedissonClient redissonClient;
+
+	@Test
+	public void simple() {
+		RQueue<Object> queue = redissonClient.getQueue("simple");
+
+		new Thread(() -> {
+			for (int i = 0; i < 50; i++) {
+				queue.add(i);
+			}
+		}).start();
+		for (int i = 0; i < 50; i++) {
+			Object poll = queue.poll();
+			if (poll == null) {
+				i--;
+			} else {
+				System.out.println(poll);
+			}
+		}
+	}
+
+	@Test
+	public void blockQueue() throws InterruptedException {
+		RBlockingQueue<Object> queue = redissonClient.getBlockingQueue("block");
+
+		new Thread(() -> {
+			for (int i = 0; i < 50; i++) {
+				try {
+					Thread.sleep(100);
+					queue.put(i);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}).start();
+		for (int i = 0; i < 50; i++) {
+			// 阻塞take
+			Object poll = queue.take();
+			System.out.println(poll);
+		}
+	}
+
+	@Test
+	public void delayQueue() throws InterruptedException {
+		RBlockingQueue<Object> blockingQueue = redissonClient.getBlockingQueue("delayQueue1");
+		RDelayedQueue<Object> delayQueue = redissonClient
+				.getDelayedQueue(blockingQueue);
+		for (int i = 0; i < 50; i++) {
+			delayQueue.offerAsync(i, (500 - i * 10), TimeUnit.MILLISECONDS);
+		}
+
+		blockingQueue = redissonClient.getBlockingQueue("delayQueue");
+		System.out.println(blockingQueue.size());
+		redissonClient.getDelayedQueue(blockingQueue);
+		for (int i = 0; i < blockingQueue.size(); i++) {
+			Object poll = blockingQueue.take();
+			System.out.println(poll);
+		}
+	}
+}
