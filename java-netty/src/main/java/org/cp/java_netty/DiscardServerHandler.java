c30b933a593cf4efbf0ecb78d34b016e5d246187
chengpeng <chengpengyeah@126.com>
2022-11-02 17:03:18 +0800

    Java之Netty

--- java-netty/src/main/java/org/cp/java_netty/DiscardServerHandler.java
+++ java-netty/src/main/java/org/cp/java_netty/DiscardServerHandler.java
@@ -9,14 +9,16 @@ public class DiscardServerHandler extends ChannelInboundHandlerAdapter {
 
 	@Override
 	public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-		ByteBuf in = (ByteBuf) msg;
+//		ByteBuf in = (ByteBuf) msg;
 		try {
-			while (in.isReadable()) { // (1)
-				System.out.print((char) in.readByte());
-				System.out.flush();
-			}
+			ctx.write(msg);
+			ctx.flush();
+//			while (in.isReadable()) { // (1)
+//				System.out.print((char) in.readByte());
+//				System.out.flush();
+//			}
 		} finally {
-			ReferenceCountUtil.release(msg); // (2)
+//			ReferenceCountUtil.release(msg); // (2)
 		}
 	}
 
